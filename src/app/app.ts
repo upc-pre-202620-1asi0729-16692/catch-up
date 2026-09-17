a2a9fb013fd9072729d7a2da41129a0ce129eb3f
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-17 10:31:50 -0500

    feat(main): add main layout to application component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
+import {Layout} from './shared/presentation/components/layout/layout';
 
 @Component({
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
+  imports: [
+    Layout
+  ]
 })
 export class App {
   protected readonly title = signal('catch-up');
