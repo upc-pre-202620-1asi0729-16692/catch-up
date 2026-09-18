b439dac361c94c8df3ff1f882b66719cb9fa6111
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-18 08:26:13 -0500

    fix(sources): integrate Source List into main view

--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {LanguageSwitcher} from '../language-switcher/language-switcher';
 import {Footer} from '../footer/footer';
+import {NewsStore} from '../../../../news/application/news.store';
+import {SourceList} from '../../../../news/presentation/components/source-list/source-list';
 
 @Component({
   imports: [
@@ -15,10 +17,20 @@ import {Footer} from '../footer/footer';
     MatIconButton,
     MatIcon,
     LanguageSwitcher,
-    Footer
+    Footer,
+    SourceList
   ],
   selector: 'app-layout',
   styleUrl: './layout.css',
   templateUrl: './layout.html',
 })
-export class Layout {}
+export class Layout implements OnInit {
+
+  protected store = inject(NewsStore);
+  protected readonly sources = this.store.sources;
+
+  ngOnInit(): void {
+    this.store.loadSources();
+  }
+
+}
