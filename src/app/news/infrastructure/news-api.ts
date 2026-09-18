aa096726391d2b67c37e008c008354e39cae5666
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-18 09:00:09 -0500

    feat(articles): add behavior to get articles in News API adapter

--- src/app/news/infrastructure/news-api.ts
+++ src/app/news/infrastructure/news-api.ts
@@ -5,18 +5,29 @@ import {SourceAssembler} from './source-assembler';
 import {map, Observable} from 'rxjs';
 import {Source} from '../domain/model/source.entity';
 import {SourcesResponse} from './sources-response';
+import {Article} from '../domain/model/article.entity';
+import {TopHeadlinesResponse} from './top-headlines-response';
+import {ArticleAssembler} from './article-assembler';
 
 @Service()
 export class NewsApi {
   private baseUrl = environment.newsProviderApiBaseUrl;
   private sourcesEndpoint = environment.newsProviderSourcesEndpointPath;
+  private newsEndpoint = environment.newsProviderNewsEndpointPath;
   private apiKey = environment.newsProviderApiKey;
   private http = inject(HttpClient);
+  private articleAssembler = inject(ArticleAssembler);
   private sourceAssembler = inject(SourceAssembler);
 
   getSources(): Observable<Source[]> {
     return this.http.get<SourcesResponse>(`${this.baseUrl}${this.sourcesEndpoint}`, {
       params: { apiKey: this.apiKey}
     }).pipe(map(response => this.sourceAssembler.toEntitiesFromResponse(response)));
+  }
+
+  getArticlesBySourceId(sourceId: string): Observable<Article[]> {
+    return this.http.get<TopHeadlinesResponse>(`${this.baseUrl}${this.newsEndpoint}`, {
+      params: { apiKey: this.apiKey, sources: sourceId }
+    }).pipe(map(response => this.articleAssembler.toEntitiesFromResponse(response)));
   }
 }
