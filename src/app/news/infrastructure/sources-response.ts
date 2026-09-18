5c0b41376127d1e456b7fe8ff6bc737f9ca06d0d
Angel Velasquez <pcisavel@upc.edu.pe>
2026-09-18 07:33:10 -0500

    feat(sources): add Sources Response and Resource

---
+++ src/app/news/infrastructure/sources-response.ts
@@ -0,0 +1,14 @@
+export interface SourcesResponse {
+  status: string;
+  sources: SourceResource[];
+}
+
+export interface SourceResource {
+  id: string;
+  name: string;
+  url: string;
+  urlToLogo: string;
+  category: string;
+  language: string;
+  country: string;
+}
